44ab959a88ff2d9211556d6223a29b17b7011f9e
root <root@iZwz9innyuqc4m9oss6qtuZ.(none)>
2017-01-07 22:25:41 +0800

    first commit

--- svrx/server/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
-var path = require('path'); 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
-
-app.use(express.static(path.join(__dirname, '../server')));
-app.listen(80);
